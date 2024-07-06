dfc82e9671b60178caa1ff9d68b366b1396accf0
Carlos Eduardo Witte <cewitte@icloud.com>
2024-07-06 20:17:33 -0300

    Initial Commit

---
+++ ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by Carlos Eduardo Witte on 06/07/24.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
